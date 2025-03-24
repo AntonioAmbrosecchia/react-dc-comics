b96b62364125b3050fa0d59a78f9fcaad477fc46
“AntonioAmbrosecchia” <antonyambro96@gmail.com>
2025-03-25 00:09:56 +0100

    end header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,23 +3,43 @@ import React from "react";
 export const Header = () => {
     return (
         <header>
-            <div>
-                <img id="logo" src="./dc-comics-1/img/dc-logo.png" ></img>
-            </div>
-            <div>
-                <div>
-                    <p></p>
-                    <p></p>
-                    <p></p>
-                    <p></p>
-                    <p></p>
-                    <p></p>
-                    <p></p>
-                    <p></p>
-                    <p></p>
-                    <p></p>
+
+            <img id="logo" src="/img/dc-logo.png" alt="Logo DC"></img>
+
+            <div id="container_menu">
+                <div class="menu">
+                    <a href="#" >CARACHTERS</a>
                 </div>
+                <div class="menu">
+                    <a href="#">COMICS</a>
+                </div>
+                <div class="menu">
+                    <a href="#">MOVIES</a>
+                </div>
+                <div class="menu">
+                    <a href="#">TV</a>
+                </div>
+                <div class="menu">
+                    <a href="#">GAMES</a>
+                </div>
+                <div class="menu">
+                    <a href="#">COLLECTIBLES</a>
+                </div>
+                <div class="menu">
+                    <a href="#">VIDEOS</a>
+                </div>
+                <div class="menu">
+                    <a href="#">FANS</a>
+                </div>
+                <div class="menu">
+                    <a href="#">NEWS</a>
+                </div>
+                <div class="menu">
+                    <a href="#">SHOP</a>
+                </div>
+
             </div>
-        </header >
+        </header>
+
     );
 };
